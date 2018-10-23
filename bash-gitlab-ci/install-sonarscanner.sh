e595f44ceb8841d5dd9f8d23b9b7952ffaba731c
David Lechner <david.lechner@talsen.team>
2018-10-23 11:43:41 +0200

    remove caveman debug

--- bash-gitlab-ci/install-sonarscanner.sh
+++ bash-gitlab-ci/install-sonarscanner.sh
@@ -6,31 +6,4 @@ export PATH="$PATH:/root/.dotnet/tools"                \
    && dotnet tool install --global dotnet-sonarscanner \
    && dotnet sonarscanner begin /k:"oppo-terminal" /d:sonar.host.url="https://sonarqube.talsen.team" /d:sonar.cs.opencover.reportsPaths="oppo-terminal.tests\coverage.opencover.xml" \
    && dotnet build oppo-terminal.sln                   \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && echo "---" \
-   && ls -Al /root/.dotnet/tools/.store/dotnet-sonarscanner/4.4.2/dotnet-sonarscanner/4.4.2/tools/netcoreapp2.1/any/sonar-scanner-3.2.0.1227/bin/sonar-scanner \
    && dotnet sonarscanner end
