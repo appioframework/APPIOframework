2a5190a9fce3eec89a0b2e1afff5c5dad7770b61
David Lechner <david.lechner@talsen.team>
2018-10-23 12:05:16 +0200

    refactoring (use pre-installed sonarscanner)

--- bash-gitlab-ci/install-sonarscanner.sh
+++ bash-gitlab-ci/install-sonarscanner.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 export PATH="$PATH:/root/.dotnet/tools"                \
-   && dotnet tool install --global dotnet-sonarscanner \
    && dotnet sonarscanner begin /k:"oppo-terminal" /d:sonar.host.url="https://sonarqube.talsen.team" /d:sonar.cs.opencover.reportsPaths="oppo-terminal.tests/coverage.opencover.xml" \
    && dotnet build oppo-terminal.sln                   \
    && dotnet sonarscanner end
