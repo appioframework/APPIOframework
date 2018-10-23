028dd1e59b7c34fc6010767b50b55a5b8335e627
David Lechner <david.lechner@talsen.team>
2018-10-23 12:04:16 +0200

    fix sonarscanner analysis (replace \ with / in coverage report path)

--- bash-gitlab-ci/install-sonarscanner.sh
+++ bash-gitlab-ci/install-sonarscanner.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 export PATH="$PATH:/root/.dotnet/tools"                \
    && dotnet tool install --global dotnet-sonarscanner \
-   && dotnet sonarscanner begin /k:"oppo-terminal" /d:sonar.host.url="https://sonarqube.talsen.team" /d:sonar.cs.opencover.reportsPaths="oppo-terminal.tests\coverage.opencover.xml" \
+   && dotnet sonarscanner begin /k:"oppo-terminal" /d:sonar.host.url="https://sonarqube.talsen.team" /d:sonar.cs.opencover.reportsPaths="oppo-terminal.tests/coverage.opencover.xml" \
    && dotnet build oppo-terminal.sln                   \
    && dotnet sonarscanner end
