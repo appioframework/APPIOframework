b803f17779a098bf5a82e853c48f12187fe2a9e0
David Lechner <david.lechner@talsen.team>
2018-10-24 08:45:39 +0200

    move source code to directory src

--- bash-gitlab-ci/run-sonarscanner.sh
+++ bash-gitlab-ci/run-sonarscanner.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 export PATH="$PATH:/root/.dotnet/tools"                \
-   && dotnet sonarscanner begin /k:"oppo-terminal" /d:sonar.host.url="https://sonarqube.talsen.team" /d:sonar.cs.opencover.reportsPaths="oppo-terminal.tests/coverage.opencover.xml" \
+   && dotnet sonarscanner begin /k:"oppo-terminal" /d:sonar.host.url="https://sonarqube.talsen.team" /d:sonar.cs.opencover.reportsPaths="src/oppo-terminal.tests/coverage.opencover.xml" \
    && dotnet build oppo-terminal.sln                   \
    && dotnet sonarscanner end
