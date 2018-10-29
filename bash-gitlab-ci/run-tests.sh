50502efc7cb506fabe96fd46f82e6381dd99f629
David Lechner <david.lechner@talsen.team>
2018-10-29 10:39:17 +0100

    remove caveman debug code

--- bash-gitlab-ci/run-tests.sh
+++ bash-gitlab-ci/run-tests.sh
@@ -4,5 +4,3 @@ set -euo pipefail
 
 dotnet test -c Release oppo-terminal.tests.sln /p:CollectCoverage=true /p:CoverletOutputFormat=opencover \
  | grep "Total Line: *"
-
-ls -Al src/oppo-terminal.tests/bin/Release/netcoreapp2.1
