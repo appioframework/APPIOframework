6600f5408482ec6b779fe0c770a1a7e9488eb943
David Lechner <david.lechner@talsen.team>
2018-10-29 10:34:25 +0100

    check if Release version was built and tested

--- bash-gitlab-ci/run-tests.sh
+++ bash-gitlab-ci/run-tests.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 
 dotnet test -c Release oppo-terminal.tests.sln /p:CollectCoverage=true /p:CoverletOutputFormat=opencover \
  | grep "Total Line: *"
+
+ls -Al src/oppo-terminal.tests/bin/Release
