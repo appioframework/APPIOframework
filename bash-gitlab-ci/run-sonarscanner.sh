db37209c048028a800a3ed5244af4a0508e99552
David Lechner <david.lechner@talsen.team>
2018-10-30 14:04:03 +0100

    update gitlab ci command run-sonarscanner (let pipeline fail if quality gate is not met)

--- bash-gitlab-ci/run-sonarscanner.sh
+++ bash-gitlab-ci/run-sonarscanner.sh
@@ -2,7 +2,14 @@
 
 set -euo pipefail
 
-export PATH="$PATH:/root/.dotnet/tools"                \
-   && dotnet sonarscanner begin /k:"oppo-terminal" /d:sonar.host.url="https://sonarqube.talsen.team" /d:sonar.cs.opencover.reportsPaths="src/oppo-terminal.tests/coverage.opencover.xml" \
-   && dotnet build oppo-terminal.sln                   \
+export PATH="$PATH:/root/.dotnet/tools"                                                                       \
+   && dotnet sonarscanner begin                                                                               \
+                          /k:"oppo-terminal"                                                                  \
+                          /d:sonar.host.url="https://sonarqube.talsen.team"                                   \
+                          /d:sonar.gitlab.commit_sha=${CI_COMMIT_SHA}                                         \
+                          /d:sonar.gitlab.ref_name=${CI_COMMIT_REF_NAME}                                      \
+                          /d:sonar.gitlab.project_id=${CI_PROJECT_ID}                                         \
+                          /d:sonar.gitlab.failure_notification_mode=exit-code                                 \
+                          /d:sonar.cs.opencover.reportsPaths="src/oppo-terminal.tests/coverage.opencover.xml" \
+   && dotnet build oppo-terminal.sln                                                                          \
    && dotnet sonarscanner end
