acc8206a294f1e7cecf3f807cdf974b9b17f39c6
David Lechner <david.lechner@talsen.team>
2018-10-30 14:31:03 +0100

    update gitlab ci command run-sonarscanner (use sonar auth for project)

--- bash-gitlab-ci/run-sonarscanner.sh
+++ bash-gitlab-ci/run-sonarscanner.sh
@@ -6,10 +6,12 @@ export PATH="$PATH:/root/.dotnet/tools"                                         
    && dotnet sonarscanner begin                                                                               \
                           /k:"oppo-terminal"                                                                  \
                           /d:sonar.host.url="https://sonarqube.talsen.team"                                   \
+                          /d:sonar.login=${SONAR_LOGIN}                                                       \
                           /d:sonar.gitlab.commit_sha=${CI_COMMIT_SHA}                                         \
                           /d:sonar.gitlab.ref_name=${CI_COMMIT_REF_NAME}                                      \
                           /d:sonar.gitlab.project_id=${CI_PROJECT_ID}                                         \
                           /d:sonar.gitlab.failure_notification_mode=exit-code                                 \
                           /d:sonar.cs.opencover.reportsPaths="src/oppo-terminal.tests/coverage.opencover.xml" \
    && dotnet build oppo-terminal.sln                                                                          \
-   && dotnet sonarscanner end
+   && dotnet sonarscanner end                                                                                 \
+                          /d:sonar.login=d4b89ca5e7fc262d936c0cc4fa85f733e6c3ce02
