76547ea8a9ad002c86fb5392aabbfef59186ac62
David Lechner <david.lechner@talsen.team>
2018-10-31 13:48:51 +0100

    add mini open62541 server as resources DLL

---
+++ src/oppo-resources/open62541/main.c
@@ -0,0 +1,21 @@
+#include "open62541.h"
+#include <signal.h>
+
+UA_Boolean running = true;
+static void stopHandler(int sig) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
+    running = false;
+}
+
+int main(void) {
+    signal(SIGINT, stopHandler);
+    signal(SIGTERM, stopHandler);
+
+    UA_ServerConfig *config = UA_ServerConfig_new_default();
+    UA_Server *server = UA_Server_new(config);
+
+    UA_StatusCode retval = UA_Server_run(server, &running);
+    UA_Server_delete(server);
+    UA_ServerConfig_delete(config);
+    return (int)retval;
+}
