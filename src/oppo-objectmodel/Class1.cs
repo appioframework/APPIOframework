984b3393ebf51599762fa35aeadac4a92227198c
Rafal Slany <rafal.slany@talsen.team>
2018-10-31 12:41:01 +0100

    provoke break line coverage

--- src/oppo-objectmodel/Class1.cs
+++ src/oppo-objectmodel/Class1.cs
@@ -1,6 +1,14 @@
 public class Class1
 {
     public Class1()
-    {        
+    {
+    }
+
+    public void Foo()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            
+        }
     }
 }
