7ee832ca6094d8cfb7972cb1a30d63603f83b738
Rafal Slany <rafal.slany@hotmail.com>
2018-11-05 19:01:08 +0100

    add missing file

---
+++ src/oppo-objectmodel/CommandStrategies/BuildCommands/BuildStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Oppo.ObjectModel.CommandStrategies.BuildCommands
+{
+    public class BuildStrategy : ICommandStrategy
+    {
+        public string Execute(IEnumerable<string> inputsArgs)
+        {
+            var firstInputParam = inputsArgs.FirstOrDefault();
+
+            if (firstInputParam == Constants.BuildCommandArguments.ModeAll)
+            {
+                return Constants.CommandResults.Success;
+            }
+
+            if (firstInputParam == Constants.BuildCommandArguments.ModelRebuild)
+            {
+                return Constants.CommandResults.Success;
+            }
+
+            return Constants.CommandResults.Failure;
+        }
+    }
+}
