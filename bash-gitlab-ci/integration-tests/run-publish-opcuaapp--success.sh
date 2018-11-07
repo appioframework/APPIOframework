c7285277b432bbf78c38d199422e46b76ad48c51
David Lechner <david.lechner@talsen.team>
2018-11-07 10:57:58 +0100

    add integration tests for oppo publish opcuaapp command

---
+++ bash-gitlab-ci/integration-tests/run-publish-opcuaapp--success.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir publish-opcuaapp--success
+cd    publish-opcuaapp--success
+
+oppo new opcuaapp --name "my-app"
+oppo build -n "my-app"
+
+if [ "${1}" = "verbose" ];
+then
+  oppo publish --name "my-app"
+else
+  oppo publish -n "my-app"
+fi
+
+if [ ! -f "my-app/publish/app" ];
+then
+  echo "published application file is missing ..."
+  exit 1
+fi
+
+if [[ ! -x "my-app/publish/app" ]]
+then
+  echo "published application file is not executable ..."
+  exit 1
+fi
+
+cd ..
+rm -rf publish-opcuaapp--success
