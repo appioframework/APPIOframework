44f3bf6fc9ad8ecbffea4d54b5d571654a22181a
Rafal Slany <rafal.slany@hotmail.com>
2018-11-13 15:04:13 +0100

    TaskId #222 - added missing file

---
+++ src/oppo-resources/text/logging/LoggingText.cs
@@ -0,0 +1,7 @@
+﻿namespace Oppo.Resources.text.logging
+{
+    public static class LoggingText
+    {
+        public const string NullInputParams_Msg = "Null input parameters passed. Specify any valid parameters.";
+    }
+}
