66a0b83e9f675305e4bfec8acc19bb013cdc1ac4
Rafal Slany <rafal.slany@hotmail.com>
2018-11-13 15:07:37 +0100

    TaskId #222 - exclude non-testable wrapper code from analysis

--- src/oppo-objectmodel/LoggerWrapper.cs
+++ src/oppo-objectmodel/LoggerWrapper.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
 
 namespace Oppo.ObjectModel
 {
+    [ExcludeFromCodeCoverage]
     public class LoggerWrapper : ILogger
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
