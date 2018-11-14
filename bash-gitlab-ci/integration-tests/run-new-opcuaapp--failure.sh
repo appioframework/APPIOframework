03d29bf5d507d202ab1b7eeb03f3da0b092ae66e
David Lechner <david.lechner@talsen.team>
2018-11-14 18:01:04 +0100

    #219 adjust integration test for bad case

--- bash-gitlab-ci/integration-tests/run-new-opcuaapp--failure.sh
+++ bash-gitlab-ci/integration-tests/run-new-opcuaapp--failure.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-set -euo pipefail
+set -uo pipefail
 
 mkdir new-opcuaapp--failure
 cd    new-opcuaapp--failure
 
 oppo new opcuaapp --name "my/\-app"
 
+if [ ${?} = 0 ];
+then
+  echo "failing command did not result in exit code != 0 ..."
+  exit 1
+fi
+
 if [ ! -f "oppo.log" ];
 then
   echo "no log entry was created ..."
@@ -16,16 +22,16 @@ fi
 cd ..
 rm -rf new-opcuaapp--failure
 
-mkdir new-opcuaapp--failure
-cd    new-opcuaapp--failure
+# mkdir new-opcuaapp--failure
+# cd    new-opcuaapp--failure
 
-oppo new opcuaapp -n "my/\-app"
+# oppo new opcuaapp -n "my/\-app"
 
-if [ ! -f "oppo.log" ];
-then
-  echo "no log entry was created ..."
-  exit 1
-fi
+# if [ ! -f "oppo.log" ];
+# then
+#   echo "no log entry was created ..."
+#   exit 1
+# fi
 
-cd ..
-rm -rf new-opcuaapp--failure
+# cd ..
+# rm -rf new-opcuaapp--failure
