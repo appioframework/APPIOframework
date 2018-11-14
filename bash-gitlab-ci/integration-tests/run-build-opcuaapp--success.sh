07f9296105ad871bd7c5cdc0906fc8de6340bbdc
David Lechner <david.lechner@talsen.team>
2018-11-14 19:57:03 +0100

    #223 extend good case integration test for existing log file check

--- bash-gitlab-ci/integration-tests/run-build-opcuaapp--success.sh
+++ bash-gitlab-ci/integration-tests/run-build-opcuaapp--success.sh
@@ -6,6 +6,7 @@ mkdir build-opcuaapp--success
 cd    build-opcuaapp--success
 
 oppo new opcuaapp -n "my-app"
+rm --force "oppo.log"
 
 if [ "${1}" = "verbose" ];
 then
@@ -23,6 +24,12 @@ fi
 if [[ ! -x "my-app/build/app" ]]
 then
   echo "deployable application file is not executable ..."
+  exit 1
+fi
+
+if [ ! -f "oppo.log" ];
+then
+  echo "no log entry was created ..."
   exit 1
 fi
 
