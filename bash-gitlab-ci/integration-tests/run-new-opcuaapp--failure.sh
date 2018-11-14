e53501b1be560a9fa3d53896a150b6e8d7a251ad
David Lechner <david.lechner@talsen.team>
2018-11-14 19:09:13 +0100

    #219 refactor integration tests

--- bash-gitlab-ci/integration-tests/run-new-opcuaapp--failure.sh
+++ bash-gitlab-ci/integration-tests/run-new-opcuaapp--failure.sh
@@ -2,36 +2,35 @@
 
 set -uo pipefail
 
-mkdir new-opcuaapp--failure
-cd    new-opcuaapp--failure
+VAR_COMMANDS[0]="oppo new opcuaapp --name \"my/\-app"\"
+VAR_COMMANDS[1]="oppo new opcuaapp -n     \"my/\-app"\"
 
-oppo new opcuaapp --name "my/\-app"
+echo ${#VAR_COMMANDS[@]}
+for INDEX in "${!VAR_COMMANDS[@]}";
+do
+  VAR_COMMAND=${VAR_COMMANDS[INDEX]}
+  
+  echo "Testing command '${VAR_COMMAND}' ..."
 
-if [ ${?} = 0 ];
-then
-  echo "failing command did not result in exit code != 0 ..."
-  exit 1
-fi
+  mkdir new-opcuaapp--failure
+  cd    new-opcuaapp--failure
 
-if [ ! -f "oppo.log" ];
-then
-  echo "no log entry was created ..."
-  exit 1
-fi
+  ${VAR_COMMAND}
 
-cd ..
-rm -rf new-opcuaapp--failure
+  if [ ${?} = 0 ];
+  then
+    echo "failing command did not result in exit code != 0 ..."
+    exit 1
+  fi
 
-# mkdir new-opcuaapp--failure
-# cd    new-opcuaapp--failure
+  if [ ! -f "oppo.log" ];
+  then
+    echo "no log entry was created ..."
+    exit 1
+  fi
 
-# oppo new opcuaapp -n "my/\-app"
+  cd ..
+  rm -rf new-opcuaapp--failure
 
-# if [ ! -f "oppo.log" ];
-# then
-#   echo "no log entry was created ..."
-#   exit 1
-# fi
-
-# cd ..
-# rm -rf new-opcuaapp--failure
+  echo "Testing command '${VAR_COMMAND}' ... done"
+done
