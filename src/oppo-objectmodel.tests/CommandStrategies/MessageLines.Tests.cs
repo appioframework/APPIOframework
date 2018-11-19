0a87fef0c992ceb0991bcb18998ba7b61eaeff52
Rafal Slany <rafal.slany@hotmail.com>
2018-11-19 16:26:54 +0100

    #197 add more unit tests

--- src/oppo-objectmodel.tests/CommandStrategies/MessageLines.Tests.cs
+++ src/oppo-objectmodel.tests/CommandStrategies/MessageLines.Tests.cs
@@ -1,11 +1,11 @@
 using System.Collections;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Oppo.ObjectModel.Tests
 {
     public class MessageLinesTests
     {
-
         [Test]
         public void ShouldInstatianteObject()
         {
@@ -16,6 +16,46 @@ namespace Oppo.ObjectModel.Tests
 
             // Assert
             Assert.IsNotNull(messageLine.GetEnumerator());
+        }
+
+        [Test]
+        public void ShouldTestCopyConstructor_OK()
+        {
+            // Arrange
+            var msgKey = "key";
+            var msgValue = "value";
+            var messageLineToCopy = new MessageLines() { { msgKey, msgValue } };
+
+            // Act
+            var messageLine = new MessageLines(messageLineToCopy);
+
+            // Assert
+            Assert.IsNotNull(messageLine);
+            Assert.AreEqual(1, messageLine.Count());
+            Assert.IsTrue(messageLine.All(v => v.Key.Contains(msgKey)));
+            Assert.IsTrue(messageLine.All(v => v.Value.Contains(msgValue)));
+        }
+
+        [Test]
+        public void ShouldTestAdd_OK()
+        {
+            // Arrange
+            var msgKey = "key";
+            var msgValue = "value";
+
+            var messageLine = new MessageLines();
+            var counBeforeAdd = messageLine.Count();
+
+            // Act
+            messageLine.Add(msgKey, msgValue);
+            var counAfterAdd = messageLine.Count();
+
+            // Assert
+            Assert.IsNotNull(messageLine);
+            Assert.AreEqual(0, counBeforeAdd);
+            Assert.AreEqual(1, counAfterAdd);
+            Assert.IsTrue(messageLine.All(v => v.Key.Contains(msgKey)));
+            Assert.IsTrue(messageLine.All(v => v.Value.Contains(msgValue)));
         }
     }
 }
