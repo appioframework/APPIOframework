d301a5942bd2899b1ed007425ecca5b917043f39
David Lechner <david.lechner@talsen.team>
2018-11-22 10:34:52 +0100

    #289 fix issue for oppo deploy

--- src/oppo-objectmodel.tests/CommandStrategies/DeployStrategy.Tests.cs
+++ src/oppo-objectmodel.tests/CommandStrategies/DeployStrategy.Tests.cs
@@ -8,6 +8,15 @@ namespace Oppo.ObjectModel.Tests.CommandStrategies
 {
     public class DeployStrategyTests
     {
+        private static string[][] ValidData()
+        {
+            return new[]
+            {
+                new string[0],
+                new[] {"--any-name", "any", "sub", "parameters"}
+            };
+        }
+
         [Test]
         public void DeployStrategy_Should_ImplementICommandOfObjectModel()
         {
@@ -50,24 +59,21 @@ namespace Oppo.ObjectModel.Tests.CommandStrategies
         }
 
         [Test]
-        public void DeployStrategy_Should_ExecuteCorrectSubCommandByName()
+        public void DeployStrategy_Should_ExecuteCorrectSubCommandByName([ValueSource(nameof(ValidData))] string[] inputParams)
         {
             // Arrange
-            const string commandName = "--any-name";
             const string expectedCommandResult = "any-result";
-            var commandInputParamsMock = new[] {commandName, "any", "sub", "parameters"};
-            var subCommandInputParamsMock = new[] {"any", "sub", "parameters"};
 
             var commandMock = new Mock<ICommand<DeployStrategy>>();
-            commandMock.Setup(x => x.Execute(It.Is<IEnumerable<string>>(p => p.SequenceEqual(subCommandInputParamsMock)))).
+            commandMock.Setup(x => x.Execute(It.Is<IEnumerable<string>>(p => p.SequenceEqual(inputParams.Skip(1).ToArray())))).
                 Returns(new CommandResult(true, new MessageLines() { { expectedCommandResult, string.Empty } }));
 
             var commandFactoryMock = new Mock<ICommandFactory<DeployStrategy>>();
-            commandFactoryMock.Setup(x => x.GetCommand(commandName)).Returns(commandMock.Object);
+            commandFactoryMock.Setup(x => x.GetCommand(inputParams.FirstOrDefault())).Returns(commandMock.Object);
             var objectUnderTest = new DeployStrategy(commandFactoryMock.Object);
 
             // Act
-            var result = objectUnderTest.Execute(commandInputParamsMock);
+            var result = objectUnderTest.Execute(inputParams);
 
             // Assert
             Assert.IsTrue(result.Sucsess);
--- src/oppo-objectmodel/CommandStrategies/DeployCommands/DeployStrategy.cs
+++ src/oppo-objectmodel/CommandStrategies/DeployCommands/DeployStrategy.cs
@@ -17,7 +17,7 @@ namespace Oppo.ObjectModel.CommandStrategies.DeployCommands
         public CommandResult Execute(IEnumerable<string> inputParams)
         {
             var inputParamsArray = inputParams.ToArray();
-            var commandName = inputParamsArray.ElementAt(0);
+            var commandName = inputParamsArray.ElementAtOrDefault(0);
             var commandParams = inputParamsArray.Skip(1).ToArray();
             var command = _factory.GetCommand(commandName);
             var result = command.Execute(commandParams);
