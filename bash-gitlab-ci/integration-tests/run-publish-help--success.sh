5fbca9b15975ff7bdc231fa1ccce72b4bd77a3c9
David Lechner <david.lechner@talsen.team>
2018-12-06 13:09:43 +0100

    #300 refactoring

--- bash-gitlab-ci/integration-tests/run-publish-help--success.sh
+++ bash-gitlab-ci/integration-tests/run-publish-help--success.sh
@@ -2,21 +2,29 @@
 
 set -euo pipefail
 
-mkdir publish-help--success
-cd    publish-help--success
+source bash-gitlab-ci/util-integration-tests.sh
 
-if [ "${1}" = "verbose" ];
-then
-  oppo publish --help
-else
-  oppo publish -h
-fi
+VAR_COMMANDS[0]="oppo publish --help"
+VAR_COMMANDS[1]="oppo publish -h"
+VAR_COMMANDS[2]="oppo publish"
 
-if [ ! -f "./oppo.log" ];
-then
-  echo "no log entry was created ..."
-  exit 1
-fi
+for INDEX in "${!VAR_COMMANDS[@]}";
+do
+  VAR_COMMAND=${VAR_COMMANDS[INDEX]}
+  
+  echo "Testing command '${VAR_COMMAND}' ..."
 
-cd ..
-rm -rf publish-help--success
+  mkdir publish-help--success
+  cd    publish-help--success
+
+  precondition_oppo_log_file_is_not_existent
+
+  ${VAR_COMMAND}
+
+  check_for_exisiting_oppo_log_file
+
+  cd ..
+  rm -rf publish-help--success
+
+  echo "Testing command '${VAR_COMMAND}' ... done"
+done
