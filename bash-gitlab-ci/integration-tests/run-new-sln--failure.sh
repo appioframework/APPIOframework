71bf1020481db7410d868f400c73de7f8810c313
David Lechner <david.lechner@talsen.team>
2018-12-06 13:06:28 +0100

    #300 refactoring

--- bash-gitlab-ci/integration-tests/run-new-sln--failure.sh
+++ bash-gitlab-ci/integration-tests/run-new-sln--failure.sh
@@ -18,19 +18,13 @@ do
   mkdir new-sln--failure
   cd    new-sln--failure
 
+  precondition_oppo_log_file_is_not_existent
+
   ${VAR_COMMAND}
 
-  if [ ${?} = 0 ];
-  then
-    echo "failing command did not result in exit code != 0 ..."
-    exit 1
-  fi
-
-  if [ ! -f "./oppo.log" ];
-  then
-    echo "no log entry was created ..."
-    exit 1
-  fi
+  check_for_non_zero_error_code
+  
+  check_for_exisiting_oppo_log_file
 
   cd ..
   rm -rf new-sln--failure
