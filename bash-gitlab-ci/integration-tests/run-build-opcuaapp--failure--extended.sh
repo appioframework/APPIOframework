997fe23e73e6ae927e6f7ac9593c73d4e6429285
David Lechner <david.lechner@talsen.team>
2018-12-07 10:47:25 +0100

    #300 added failure case integration test for oppo build

--- bash-gitlab-ci/integration-tests/run-build-opcuaapp--failure--extended.sh
+++ bash-gitlab-ci/integration-tests/run-build-opcuaapp--failure--extended.sh
@@ -38,3 +38,40 @@ do
 done
 
 echo "Testing failure of meson call ... done"
+
+# ---
+
+VAR_COMMANDS[0]="oppo build --name my-app"
+VAR_COMMANDS[1]="oppo build -n     my-app"
+
+echo "Testing failure of ninja call ..."
+
+for INDEX in "${!VAR_COMMANDS[@]}";
+do
+  VAR_COMMAND=${VAR_COMMANDS[INDEX]}
+  
+  echo "Testing command '${VAR_COMMAND}' ..."
+
+  mkdir build-opcuaapp--failure--extended
+  cd    build-opcuaapp--failure--extended
+
+  oppo new opcuaapp --name my-app
+  rm --force "./oppo.log"
+
+  echo "// removed source code for this test case ..." > "./my-app/src/server/main.c"
+
+  precondition_oppo_log_file_is_not_existent
+
+  ${VAR_COMMAND}
+
+  check_for_non_zero_error_code
+
+  check_for_exisiting_oppo_log_file
+
+  cd     ..
+  rm -rf build-opcuaapp--failure--extended
+
+  echo "Testing command '${VAR_COMMAND}' ... done"
+done
+
+echo "Testing failure of ninja call ... done"
