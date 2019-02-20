951edb033c54856b3866e46c762019f2e024b68e
kamil.wilkosz <kamil.wilkosz@talsen.team>
2019-02-20 12:54:54 +0100

    #806 deserializer exception

--- src/oppo-objectmodel/CommandStrategies/SlnCommands/SlnAddCommandStrategy.cs
+++ src/oppo-objectmodel/CommandStrategies/SlnCommands/SlnAddCommandStrategy.cs
@@ -77,7 +77,7 @@ namespace Oppo.ObjectModel.CommandStrategies.SlnCommands
                 oppoSolution = JsonConvert.DeserializeObject<Solution>(slnContent);
 				if (oppoSolution == null)
 				{
-					throw new Exception();
+					throw new Exception("Invalid deserialization object.");
 				}
             }
             catch(Exception)
@@ -99,7 +99,7 @@ namespace Oppo.ObjectModel.CommandStrategies.SlnCommands
                 oppoProj = JsonConvert.DeserializeObject<Opcuaapp>(opcuaappContent);
 				if (oppoProj == null)
 				{
-					throw new Exception();
+					throw new Exception("Invalid deserialization object.");
 				}
 			}
             catch(Exception)
