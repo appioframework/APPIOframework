ea538df9f86a727c3a0a75739b30d171d61affe4
kamil.wilkosz <kamil.wilkosz@talsen.team>
2019-02-22 08:28:53 +0100

    #782 new opcuaapp failure integration test adjusted

--- bash-gitlab-ci/integration-tests/run-new-opcuaapp--failure.sh
+++ bash-gitlab-ci/integration-tests/run-new-opcuaapp--failure.sh
@@ -2,12 +2,17 @@
 
 set -uo pipefail
 
-VAR_COMMANDS[0]="oppo new opcuaapp --name \"my/\-app\""
-VAR_COMMANDS[1]="oppo new opcuaapp -n     \"my/\-app\""
-VAR_COMMANDS[2]="oppo new opcuaapp --name"
-VAR_COMMANDS[3]="oppo new opcuaapp -n"
-VAR_COMMANDS[4]="oppo new opcuaapp --exit"
-VAR_COMMANDS[5]="oppo new opcuaapp -x"
+VAR_COMMANDS[0] ="oppo new opcuaapp --name \"my/\-app\""
+VAR_COMMANDS[1] ="oppo new opcuaapp -n     \"my/\-app\""
+VAR_COMMANDS[2] ="oppo new opcuaapp --name"
+VAR_COMMANDS[3] ="oppo new opcuaapp -n"
+VAR_COMMANDS[4] ="oppo new opcuaapp --exit"
+VAR_COMMANDS[5] ="oppo new opcuaapp -x"
+VAR_COMMANDS[6] ="oppo new opcuaapp -n my-app -type Client"
+VAR_COMMANDS[7] ="oppo new opcuaapp -n my-app --t Server"
+VAR_COMMANDS[8] ="oppo new opcuaapp -p my-app -t ClientServer"
+VAR_COMMANDS[9] ="oppo new opcuaapp -n my-app -t"
+VAR_COMMANDS[10]="oppo new opcuaapp -n my-app --type"
 
 for INDEX in "${!VAR_COMMANDS[@]}";
 do
