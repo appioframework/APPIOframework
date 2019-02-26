44d09ea442c07a0172970c6e4954305075543617
Daniela Balta <ana.anic@privatna.domena.com>
2019-02-26 16:14:02 +0100

    #812 Add Reference Strategy Command

---
+++ src/oppo-objectmodel.tests/CommandStrategies/ReferenceAddCommandStrategy.Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Oppo.ObjectModel.CommandStrategies.SlnCommands;
+using Oppo.Resources.text.output;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Oppo.ObjectModel.Tests.CommandStrategies
+{
+    class ReferenceAddCommandStrategySholud
+    {
+        protected static string[][] ValidInputs()
+        {
+            return new[]
+            {
+                new [] { "-S", "testServer", "-C", "testProj" },
+                new [] { "-S", "testSln", "--client", "testProj" },
+                new [] { "--server", "testSln", "-C", "testProj" },
+                new [] { "--server", "testSln", "--client", "testProj" },
+            };
+        }
+        protected static string[][] InvalidInputs_UnknownClientParam()
+        {
+            return new[]
+            {
+                new [] { "-S", "testSln", "-C", "testProj" },
+                new [] { "-S", "testSln", "--client", "testProj" },
+                new [] { "--server", "testSln", "-C", "testProj" },
+                new [] { "--server", "testSln", "--client", "testProj" },
+            };
+        }
+    }
+}
---
+++ src/oppo-objectmodel/CommandStrategies/ReferenceCommands/ReferenceStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Oppo.ObjectModel.CommandStrategies.ReferenceCommands
+{
+
+        public class ReferenceStrategy : ICommand<ObjectModel>
+        {
+            private readonly ICommandFactory<ReferenceStrategy> _factory;
+
+            public ReferenceStrategy(ICommandFactory<ReferenceStrategy> factory)
+            {
+                _factory = factory;
+            }
+
+            public string Name => Constants.CommandName.Reference;
+
+            public CommandResult Execute(IEnumerable<string> inputParams)
+            {
+                var inputParamsArray = inputParams.ToArray();
+                var commandName = inputParamsArray.ElementAtOrDefault(0);
+                var commandParams = inputParamsArray.Skip(1).ToArray();
+                var command = _factory.GetCommand(commandName);
+                return command.Execute(commandParams);
+            }
+
+            public string GetHelpText()
+            {
+                return Resources.text.help.HelpTextValues.ReferenceCommand;
+            }
+        }
+    
+}
