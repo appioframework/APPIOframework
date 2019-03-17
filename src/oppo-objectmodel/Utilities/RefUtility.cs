3f6a612ab8d4219649fda786f76fd4b0f8c7f367
kamil.wilkosz <kamil.wilkosz@talsen.team>
2019-03-17 13:45:20 +0100

    #812 missing file added

---
+++ src/oppo-objectmodel/Utilities/RefUtility.cs
@@ -0,0 +1,20 @@
+﻿namespace Oppo.ObjectModel
+{
+	public static class RefUtility
+	{
+		public struct ResultMessages
+		{
+			public string LoggerMessage { get; set; }
+			public string OutputMessage { get; set; }
+		};
+
+		static public void DeserializeClient(ref OpcuaClientApp opcuaClient, ref OpcuaClientServerApp opcuaClientServer, string clientFullName, IFileSystem fileSystem)
+		{
+			opcuaClient = SlnUtility.DeserializeFile<OpcuaClientApp>(clientFullName, fileSystem);
+			if (opcuaClient != null && opcuaClient.Type == Constants.ApplicationType.ClientServer)
+			{
+				opcuaClientServer = SlnUtility.DeserializeFile<OpcuaClientServerApp>(clientFullName, fileSystem);
+			}
+		}
+	}
+}
