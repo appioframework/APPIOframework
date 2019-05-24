028c122c1ba3e316a55a46037280e3cc9a91bc25
kamil.wilkosz <kamil.wilkosz@talsen.team>
2019-05-24 08:25:27 +0200

    #1076 second part of generate integration tests adjusted

--- bash-gitlab-ci/integration-tests/run-generate-information-model--success.sh
+++ bash-gitlab-ci/integration-tests/run-generate-information-model--success.sh
@@ -4,10 +4,8 @@ set -euo pipefail
 
 source bash-gitlab-ci/util-integration-tests.sh
 
-VAR_COMMANDS[0]="oppo generate information-model -n my-app -m minimumCompilableModel.xml"
-VAR_COMMANDS[1]="oppo generate information-model -n my-app --model minimumCompilableModel.xml"
-VAR_COMMANDS[2]="oppo generate information-model --name my-app -m minimumCompilableModel.xml"
-VAR_COMMANDS[3]="oppo generate information-model --name my-app --model minimumCompilableModel.xml"
+VAR_COMMANDS[0]="oppo generate information-model -n my-app"
+VAR_COMMANDS[1]="oppo generate information-model --name my-app"
 
 for INDEX in "${!VAR_COMMANDS[@]}";
 do
@@ -18,14 +16,13 @@ do
   mkdir generate-information-model--success
   cd    generate-information-model--success
 
-  oppo new opcuaapp -n "my-app" -t "ClientServer" -u "127.0.0.1" -p "4840"
+  oppo new opcuaapp -n "my-app" -t "ClientServer" -u "127.0.0.1" -p "4840" --nocert
+  echo "<UANodeSet xmlns=\"http://opcfoundation.org/UA/2011/03/UANodeSet.xsd\"><Models><Model ModelUri=\"test\" Version=\"1.01\" PublicationDate=\"2012-12-31T00:00:00Z\"></Model></Models></UANodeSet>" > minimumCompilableModel.xml
+  oppo import information-model -n "my-app" -p "minimumCompilableModel.xml"
+
   rm --force "./oppo.log"
 
   precondition_oppo_log_file_is_not_existent
-
-  cd my-app/models
-  echo "<UANodeSet xmlns=\"http://opcfoundation.org/UA/2011/03/UANodeSet.xsd\"><Models><Model ModelUri=\"test\" Version=\"1.01\" PublicationDate=\"2012-12-31T00:00:00Z\"></Model></Models></UANodeSet>" > minimumCompilableModel.xml
-  cd ../..
 
   ${VAR_COMMAND}
 
@@ -36,9 +33,6 @@ do
 
   check_for_exisiting_file_named "./my-app/src/server/information-models/minimumCompilableModel.h" \
                                  "information-model .h file does not exist ..."
-
-  check_for_exisiting_file_named "./my-app/build/server-app" \
-								 "server-app file does not exist ..."
 
   check_for_exisiting_oppo_log_file
 
