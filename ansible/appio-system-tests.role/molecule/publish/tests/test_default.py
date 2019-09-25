5007664ae747fef2b4fe2fdccf2ed63279fc1a63
David Lechner <david.lechner@talsen.team>
2019-09-25 17:39:42 +0200

    replace bash tests by ansible tests (for appio new publish)

--- ansible/appio-system-tests.role/molecule/publish/tests/test_default.py
+++ ansible/appio-system-tests.role/molecule/publish/tests/test_default.py
@@ -3,6 +3,7 @@ import os
 
 import testinfra.utils.ansible_runner
 
+from .util.prepare import assert_that_files_are_executable
 from .util.prepare import assert_that_files_are_existing
 from .util.prepare import assert_that_files_are_missing
 from .util.prepare import prepare_provide_test_directory
@@ -13,16 +14,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 @pytest.mark.parametrize('case, command', [
-    ['1', 'appio publish --help'],
-    ['2', 'appio publish -h'],
-    ['3', 'appio publish'],
+    ['1_help', 'appio publish --help'],
+    ['2_help', 'appio publish -h'],
+    ['3_help', 'appio publish'],
 ])
 def test_that_appio_publish_help_is_succeeding(host, case, command):
     # prepare
     test_dir_path = prepare_provide_test_directory(host, case)
 
     file_paths = [
-        test_dir_path + 'appio.log'
+        test_dir_path + 'appio.log',
     ]
 
     # arrange
@@ -33,6 +34,77 @@ def test_that_appio_publish_help_is_succeeding(host, case, command):
 
     # assert
     assert appio.rc == 0
+    assert appio.stdout != ''
+
+    assert_that_files_are_existing(host, file_paths)
+
+
+@pytest.mark.parametrize('case, command', [
+    ['1', 'appio publish --name my-app'],
+    ['2', 'appio publish -n     my-app'],
+])
+def test_that_appio_publish_is_succeeding(host, case, command):
+    # prepare
+    test_dir_path = prepare_provide_test_directory(host, case)
+
+    file_paths = [
+        test_dir_path + 'appio.log',
+        test_dir_path + app_name + 'publish/client-app',
+        test_dir_path + app_name + 'publish/server-app',
+    ]
+
+    exe_file_paths = [
+        file_paths[1],
+        file_paths[2],
+    ]
+
+    for prepare_command in (
+        'appio new opcuaapp -n ' + app_name + ' -t ClientServer -u 127.0.0.1 -p 4840',  # noqa: #501
+        'appio build -n ' + app_name,
+        'rm -f appio.log',
+    ):
+        prepare = host.run('cd ' + test_dir_path + ' && ' + prepare_command)
+
+        assert prepare.rc == 0
+
+    # arrange
+    assert_that_files_are_missing(host, file_paths)
+
+    # act
+    appio = host.run('cd ' + test_dir_path + ' && ' + command)
+
+    # assert
+    assert appio.rc == 0
+    assert appio.stdout != ''
+
+    assert_that_files_are_existing(host, file_paths)
+    assert_that_files_are_executable(host, exe_file_paths)
+
+
+@pytest.mark.parametrize('case, command', [
+    ['1f', 'appio publish --name my/-app'],
+    ['2f', 'appio publish -n     my/-app'],
+    ['3f', 'appio publish --name'],
+    ['4f', 'appio publish -n'],
+    ['5f', 'appio publish --exit'],
+    ['6f', 'appio publish -x'],
+])
+def test_that_appio_publish_is_failing(host, case, command):
+    # prepare
+    test_dir_path = prepare_provide_test_directory(host, case)
+
+    file_paths = [
+        test_dir_path + 'appio.log',
+    ]
+
+    # arrange
+    assert_that_files_are_missing(host, file_paths)
+
+    # act
+    appio = host.run('cd ' + test_dir_path + ' && ' + command)
+
+    # assert
+    assert appio.rc != 0
     assert appio.stdout != ''
 
     assert_that_files_are_existing(host, file_paths)
