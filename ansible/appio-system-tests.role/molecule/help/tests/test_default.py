700aabe606c5f5376d91a78602357e85a95ff57c
David Lechner <david.lechner@talsen.team>
2019-09-25 16:15:19 +0200

    refine appio help tests

--- ansible/appio-system-tests.role/molecule/help/tests/test_default.py
+++ ansible/appio-system-tests.role/molecule/help/tests/test_default.py
@@ -3,6 +3,8 @@ import os
 
 import testinfra.utils.ansible_runner
 
+from .util.prepare import assert_that_files_are_existing
+from .util.prepare import assert_that_files_are_missing
 from .util.prepare import prepare_provide_test_directory
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -26,9 +28,7 @@ def test_that_appio_help_is_succeeding(host, case, command):
     ]
 
     # arrange
-    for file_path in file_paths:
-        f = host.file(file_path)
-        assert not f.exists
+    assert_that_files_are_missing(host, file_paths)
 
     # act
     appio = host.run('cd ' + test_dir_path + ' && ' + command)
@@ -37,6 +37,4 @@ def test_that_appio_help_is_succeeding(host, case, command):
     assert appio.rc == 0
     assert appio.stdout != ''
 
-    for file_path in file_paths:
-        f = host.file(file_path)
-        assert f.exists
+    assert_that_files_are_existing(host, file_paths)
