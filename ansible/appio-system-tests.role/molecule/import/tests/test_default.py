880e7cd84a555be68afa775a7adbfaad453b188b
David Lechner <david.lechner@talsen.team>
2019-09-27 10:11:08 +0200

    add appio import test cases

---
+++ ansible/appio-system-tests.role/molecule/import/tests/test_default.py
@@ -0,0 +1,52 @@
+import pytest
+import os
+
+import testinfra.utils.ansible_runner
+
+from .util.prepare import assert_that_files_are_executable
+from .util.prepare import assert_that_files_are_existing
+from .util.prepare import assert_that_files_are_missing
+from .util.prepare import prepare_provide_test_directory
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+@pytest.mark.parametrize('case, command, app_name', [
+    ['1f', 'appio import -n my-app -p model.xml', 'my-app'],
+    ['2f', 'appio import -n my-app --path model.xml', 'my-app'],
+    ['3f', 'appio import --name my-app -p model.xml', 'my-app'],
+    ['4f', 'appio import --name --path model.xml', 'my-app'],
+    ['5f', 'appio import -n my-app -s', 'my-app'],
+    ['6f', 'appio import -n my-app --sample', 'my-app'],
+    ['7f', 'appio import --name my-app -s', 'my-app'],
+    ['8f', 'appio import --name my-app --sample', 'my-app'],
+])
+def test_that_appio_import_is_failing(host, case, command, app_name):
+    # prepare
+    test_dir_path = prepare_provide_test_directory(host, case)
+
+    file_paths = [
+        test_dir_path + 'appio.log',
+    ]
+
+    for prepare_command in (
+        'appio new opcuaapp -n ' + app_name + ' -t ClientServer -u 127.0.0.1 -p 4840',  # noqa: #501
+        'rm -f appio.log',
+    ):
+        prepare = host.run('cd ' + test_dir_path + ' && ' + prepare_command)
+
+        assert prepare.rc == 0
+
+    # arrange
+    assert_that_files_are_missing(host, file_paths)
+
+    # act
+    appio = host.run('cd ' + test_dir_path + ' && ' + command)
+
+    # assert
+    assert appio.rc != 0
+    assert appio.stdout != ''
+
+    assert_that_files_are_existing(host, file_paths)
---
+++ ansible/appio-system-tests.role/molecule/import/tests/util/prepare.py
@@ -0,0 +1,47 @@
+import pytest
+import os
+
+import testinfra.utils.ansible_runner
+
+
+def assert_that_files_are_directories(host, file_paths):
+    for file_path in file_paths:
+        f = host.file(file_path)
+
+        assert f.is_directory
+
+
+def assert_that_files_are_executable(host, file_paths):
+    for file_path in file_paths:
+        f = host.file(file_path)
+
+        assert f.mode == 0o755
+
+
+def assert_that_files_are_existing(host, file_paths):
+    for file_path in file_paths:
+        f = host.file(file_path)
+
+        assert f.exists
+
+
+def assert_that_files_are_missing(host, file_paths):
+    for file_path in file_paths:
+        f = host.file(file_path)
+
+        assert not f.exists
+
+
+def prepare_provide_test_directory(host, case):
+    test_dir_path = case + '/'
+
+    mk_test_dir = host.run('mkdir --parents ' + test_dir_path)
+
+    assert mk_test_dir.rc == 0
+
+    test_dir = host.file(test_dir_path)
+
+    assert test_dir.exists
+    assert test_dir.is_directory
+
+    return test_dir_path
