c743b396f2d762470bdd8a3573ad84d71a0d62a8
David Lechner <david.lechner@talsen.team>
2019-09-27 10:14:51 +0200

    add test cases for appio import information-model

--- ansible/appio-system-tests.role/molecule/import/tests/test_default.py
+++ ansible/appio-system-tests.role/molecule/import/tests/test_default.py
@@ -50,3 +50,42 @@ def test_that_appio_import_is_failing(host, case, command, app_name):
     assert appio.stdout != ''
 
     assert_that_files_are_existing(host, file_paths)
+
+
+@pytest.mark.parametrize('case, command, app_name', [
+    ['1f', 'appio import information-model -n my-app -p model.txt', 'my-app'],
+    ['2f', 'appio import information-model -n my-app --path model.xml2', 'my-app'],
+    ['3f', 'appio import information-model -n my-app --Path model.xml2', 'my-app'],
+    ['4f', 'appio import information-model -n my-app -P model.xml2', 'my-app'],
+    ['5f', 'appio import information-model -n my-app -p notExistingModel.xml', 'my-app'],
+    ['6f', 'appio import information-model -nn my-app -p notExistingModel.xml', 'my-app'],
+    ['7f', 'appio import information-model my-app -p notExistingModel.xml', 'my-app'],
+])
+def test_that_appio_import_information_model_is_failing(host, case, command, app_name):
+    # prepare
+    test_dir_path = prepare_provide_test_directory(host, case)
+
+    file_paths = [
+        test_dir_path + 'appio.log',
+    ]
+
+    for prepare_command in (
+        'appio new opcuaapp -n ' + app_name + ' -t ClientServer -u 127.0.0.1 -p 4840',  # noqa: #501
+        'touch model.xml',
+        'rm -f appio.log',
+    ):
+        prepare = host.run('cd ' + test_dir_path + ' && ' + prepare_command)
+
+        assert prepare.rc == 0
+
+    # arrange
+    assert_that_files_are_missing(host, file_paths)
+
+    # act
+    appio = host.run('cd ' + test_dir_path + ' && ' + command)
+
+    # assert
+    assert appio.rc != 0
+    assert appio.stdout != ''
+
+    assert_that_files_are_existing(host, file_paths)
