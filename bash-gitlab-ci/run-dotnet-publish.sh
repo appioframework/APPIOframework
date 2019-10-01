b9abd75319c871115dab50a33f7323b641ad8046
David Lechner <david.lechner@talsen.team>
2019-10-01 15:31:49 +0200

    remove unused shell scripts

--- bash-gitlab-ci/run-dotnet-publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cd src/appio-terminal
-dotnet publish -c Release -o ../../publish
--- bash-gitlab-ci/run-system-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cd ansible/appio-system-tests.role/
-
-molecule test --all
