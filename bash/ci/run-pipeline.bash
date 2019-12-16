c6e5f961738a7401ee56390a46dfa26e224be39d
David Lechner <david.lechner@talsen.team>
2019-12-16 23:00:44 +0100

    prepare stage execution via docker

--- bash/ci/run-pipeline.bash
+++ bash/ci/run-pipeline.bash
@@ -4,12 +4,28 @@ set -euo pipefail
 
 source bash/util/functions.bash
 
+function run_pipeline_stages_via_docker() {
+    local TITLE="Running stages via docker"
+
+    print_stage \
+    "${TITLE}"
+
+    print_stage \
+    "${TITLE}" \
+    "done"
+}
+
 function run_pipeline() {
-    print_title \
-    "Running CI pipeline"
+    local TITLE="Running CI pipeline"
 
     print_title \
-    "Running CI pipeline" \
+    "${TITLE}"
+
+    run_pipeline_stages_via_docker \
+    ${@}
+
+    print_title \
+    "${TITLE}" \
     "done"
 }
 
--- bash/util/functions.bash
+++ bash/util/functions.bash
@@ -8,6 +8,12 @@ function __done() {
     echo "\033[0;32mdone\033[0m"
 }
 
+function print_stage() {
+    set +u
+    echo -e " * ${1} ... $( __${2} )"
+    set -u
+}
+
 function print_title() {
     set +u
     echo -e "${1} ... $( __${2} )"
