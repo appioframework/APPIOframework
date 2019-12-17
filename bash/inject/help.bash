0715e3af471385b554fbcccfd5cd47dc0b8171a3
David Lechner <david.lechner@talsen.team>
2019-12-17 12:44:39 +0100

    fix execution directory for help injection

--- bash/inject/help.bash
+++ bash/inject/help.bash
@@ -2,7 +2,16 @@
 
 set -euo pipefail
 
+source bash/config/cache-dir.bash
+
 function help() {
+    mkdir \
+    --parents \
+    ${CACHE_DIR}
+
+    cd \
+    ${CACHE_DIR}
+
     appio \
     --help
 }
